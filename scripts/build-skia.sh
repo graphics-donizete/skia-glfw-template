a225be83a2f4e440278e912711ae5000634d16d1
Donizete Vida <donizetevida1998@outlook.com>
2025-11-12 23:41:11 +0100

    Fix compiling flags for Linux

--- scripts/build-skia.sh
+++ scripts/build-skia.sh
@@ -2,7 +2,7 @@ pushd external/skia
 
 python3 tools/git-sync-deps
 
-bin/gn gen out/Static --args='is_official_build=false'
+bin/gn gen out/Static --args='is_official_build=false skia_use_system_freetype2=false'
 
 ninja -C out/Static
 
